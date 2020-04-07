257860373eda9932fbf392f87ce4f35a14b650c7
Fadejimi Adegbulugbe <adegbulugbefadejimi@gmail.com>
2020-04-07 14:20:47 -0400

    header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppReviewMainComponent } from './modules/app-review-main/app-review-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppReviewMainComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/app-review-main/app-review-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-review-main',
+  templateUrl: './app-review-main.component.html',
+  styleUrls: ['./app-review-main.component.css']
+})
+export class AppReviewMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
