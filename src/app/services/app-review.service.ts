91eaa2c489ba1613758b42a7fafc59501e0087ad
Fadejimi Adegbulugbe <adegbulugbefadejimi@gmail.com>
2020-04-08 09:14:12 -0400

    app-review

--- src/app/services/app-review.service.ts
+++ src/app/services/app-review.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {AppDetails} from "../models/AppDetails";
+import {AppDetails} from '../models/AppDetails';
 
 @Injectable({
   providedIn: 'root'
